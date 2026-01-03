91fa7986ebd603080d8ed33af90d45dc319ace3b
just_wasif <wasif.iitr.ansari@gmail.com>
2026-01-03 14:26:45 +0530

    chore :setup vite and tailwind css

---
+++ week-1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Tailwind 3.4.18 Works 🚀
+      </h1>
+    </div>
+  );
+}
+
+
+export default App
