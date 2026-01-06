ab1df8ccba335aa444d00ce60dd984ec21460395
just_wasif <wasif.iitr.ansari@gmail.com>
2026-01-06 15:02:41 +0530

    edit:changed adress to testnet sepolia network

--- week-1/Frontend /src/contract/contract_details.js
+++ week-1/Frontend /src/contract/contract_details.js
@@ -2,6 +2,6 @@
 import abi from "./abi.json"
 
 const Contract_abi=abi;
-const Contract_Adress="0x5FbDB2315678afecb367f032d93F642f64180aa3"
-
+const Contract_Adress="0x7B97a124aCF804ae4b3Cd821D4C47495089e901A"
+//anvil 0x5FbDB2315678afecb367f032d93F642f64180aa3
 export {Contract_Adress,Contract_abi};
