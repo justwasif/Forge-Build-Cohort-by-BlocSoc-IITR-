f27aa2c295ca44f3012bbf7b5e450f24d64fdcf7
just_wasif <wasif.iitr.ansari@gmail.com>
2026-01-12 02:43:25 +0530

    feat:added frontend (no css )

--- week-2/frontend/src/App.jsx
+++ week-2/frontend/src/App.jsx
@@ -1,14 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-blue-400">
-        Tailwind 3.4.18 Works 🚀
-      </h1>
+import { useState,useEffect } from 'react';
+import { Contract_Adress,abi} from './contract/contract_details';
+import {ethers} from "ethers"
+
+function App(){
+  const [account,setAccount]=useState(",");
+  const [contract,setContract]=useState(null);
+  const [depositAmount,setDepositAmount]=useState("");
+  const [queryAddress,setQueryAddress]=useState("");
+  const [userBalance,setUserBalance]=useState("");
+  const [contractBalance,setContractBalance]=useState("");
+
+  async function connect() {
+    const provider=new ethers.BrowserProvider(window.ethereum);
+    await provider.send("eth_requestAccount",[]);
+    const singer= await provider.getSinger()
+
+    const account=await window.ethereum.request({
+      method:"eth_requestAccounts"
+    });
+
+    const ledger=new ethers.Contract(Contract_Adress,abi,singer);
+    setAccount(await singer.getAddress());
+    setContract(ledger);  
+    await provider.wait();
+    alert("connected");
+  }
+
+  async function deposite() {
+    if(!contract) return alert("bhai wallet to connect kar le");
+
+    const tx=await contract.deposite({
+      value:ether.parseEther(depositAmount),
+    });
+    await tx.wait();
+    alert("success");
+  }
+  async function getUserBalance() {
+    if(!contract) return alert("wallet");
+    const bal=await contract.userBalance(queryAddress);
+    setUserBalance(ether.formatEther(bal));
+
+
+  }
+  async function getContractBalance() {
+    if(!contract) return alert("wallet");
+    const bal=await contract.contractBalance();
+
+    setContractBalance(ethers.formatEther(bal));
+    
+  }
+
+  return(
+    <div>
+      <li>
+        <h1>Dapp Ledger</h1>
+      </li>
+      <li>
+        <button onClick={connect}>connect wallet</button>
+        <p>{account && `Connecte:${account}`}</p>
+      </li>
+      <li>
+        <h1>deposit in acc</h1>
+        <input placeholder='amount in eth' value={depositAmount} onChange={(e)=>setDepositAmount(e.target.value)}/>
+        <button onClick={deposite}>deposite</button>
+      </li>
+      <li>
+        <h1>check userBalance</h1>
+        <input placeholder='account adress' value={queryAddress} onChange={(e)=>setQueryAddress(e.target.value)}/>
+        <button onClick={getUserBalance}>check bal</button>
+        <p>{userBalance && `balance:${userBalance} eth`}</p>
+      </li>
+      <li>
+        <h1> cont bal</h1>
+        <button onClick={getContractBalance}>ledger bal</button>
+        <p>{contractBalance && `contract bal ${contractBalance} eth`}</p>
+      </li>
     </div>
   );
+
 }
+export default App;
---
+++ week-2/frontend/src/contract/contract_details.js
@@ -0,0 +1,68 @@
+//import ABI from "./LedgerAbi.json";
+
+
+
+const Contract_Adress="0x2524A762326faF1EBdDd1093F0779dfC7cCff6b1";
+const abi=[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "balance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "contractBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_userAddress",
+				"type": "address"
+			}
+		],
+		"name": "userBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export {Contract_Adress,abi};
+
+
