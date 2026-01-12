71e1797ca62ee276f5162323def8e9091411fad6
just_wasif <wasif.iitr.ansari@gmail.com>
2026-01-12 22:36:47 +0530

    fix: improve UI styling (like hover padding ,placeholder, shadow,font)

--- week-2/frontend/src/App.jsx
+++ week-2/frontend/src/App.jsx
@@ -45,79 +45,79 @@ function App(){
     setContractBalance(ethers.formatEther(bal));
   }
 
-  return (
-  <div className="min-h-screen  flex items-center justify-center">
-    <div className="border rounded-lg ">
+ return (
+  <div className="min-h-screen flex items-center justify-center">
+    <div className="bg-white w-full max-w-md p-6 rounded-xl shadow-lg">
 
-      <h1 className="text-3xl  text-center  ">
+      <h1 className="text-3xl font-bold text-center mb-6">
         DApp Ledger
       </h1>
 
-      
+    
       <div className="border rounded-lg p-4 mb-4">
         <button
           onClick={connect}
-          className="w-full bg-blue-500 text-white "
+          className="w-full bg-blue-500 hover:bg-red-600 text-white font-semibold py-2 rounded"
         >
           Connect Wallet
         </button>
         {account && (
-          <p className="mt-2 text-sm  break-all text-center">
+          <p className="mt-2 text-sm text-red-600 break-all text-center">
             Connected: {account}
           </p>
         )}
       </div>
 
-  
+      
       <div className="border rounded-lg p-4 mb-4">
-        <h3 className=" text-center">enter deposite amount eth</h3>
+        <h3 className="font-semibold mb-2 text-center">Deposit ETH</h3>
         <input
-          className="w-full border  "
-          placeholder="eth"
+          className="w-full border px-3 py-2 rounded mb-2"
+          placeholder="ETH"
           value={depositAmount}
           onChange={(e) => setDepositAmount(e.target.value)}
         />
         <button
           onClick={deposit}
-          className="w-full bg-green-500 text-white "
+          className="w-full bg-green-500 hover:bg-red-600 text-white font-semibold py-2 rounded"
         >
           Deposit
         </button>
       </div>
 
       
-      <div className="border rounded-lg ">
-        <h3 className=" text-center">Check User Balance</h3>
+      <div className="border rounded-lg p-4 mb-4">
+        <h3 className="font-semibold mb-2 text-center">Check User Balance</h3>
         <input
-          className="w-full border  rounded "
-          placeholder="account address"
+          className="w-full border px-3 py-2 rounded mb-2"
+          placeholder="Account address"
           value={queryAddress}
           onChange={(e) => setQueryAddress(e.target.value)}
         />
         <button
           onClick={getUserBalance}
-          className="w-full bg-purple-500  text-white "
+          className="w-full bg-purple-500 hover:bg-red-600 text-white font-semibold py-2 rounded"
         >
           Check Balance
         </button>
         {userBalance && (
-          <p className=" text-center">
+          <p className="mt-2  text-center">
             Balance: {userBalance} ETH
           </p>
         )}
       </div>
 
-    
-      <div className="border rounded-lg ">
-        <h3 className=" text-center">Contract Balance</h3>
+      
+      <div className="border rounded-lg p-4">
+        <h3 className="font-semibold mb-2 text-center">Contract Balance</h3>
         <button
           onClick={getContractBalance}
-          className="w-full bg-orange-500  text-white "
+          className="w-full bg-orange-500 hover:bg-red-600 text-white font-semibold py-2 rounded"
         >
           Get Ledger Balance
         </button>
         {contractBalance && (
-          <p className="  text-center">
+          <p className="mt-2   text-center">
             Contract Balance: {contractBalance} ETH
           </p>
         )}
@@ -126,6 +126,7 @@ function App(){
     </div>
   </div>
 );
+
 
 
 }
